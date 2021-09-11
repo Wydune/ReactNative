7767e8fa71c01f39ab6585b062c1230cf9cd93d4
Wydune <rafael.gavarron@gmail.com>
2021-09-11 17:26:32 -0300

    TextInput

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,11 +4,11 @@ import { TextInput, View } from "react-native"
 export default function Form(){
     return(
        <View>
-           <View>
+           <View> //serve para visualizar ;
                <Text>Altura</Text>
-               <TextInput></TextInput>
+               <TextInput placeholder="Ex. 1,75" keyboardType="numeric"/>
                <Text>Peso</Text>  
-               <TextInput></TextInput>         
+               <TextInput placeholder="Ex. 69" keyboardType="numeric"/>         
            </View>
        </View>
     );
